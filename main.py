ee1d675aeb86e3a03fe7047dee798ab96bc0c66e
AIM-Gaming <sonealpageot@gmail.com>
2025-12-12 14:14:04 -0500

    Added dynamic button box size (buttons don't respond)

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from assets import extract_video, extract_assets
 from app import BibleTriviaApp
 
 # Pressed pause button isn't the right size
+# Update the button sizes and positions on home.py (play, resume, options, credits)
+# Update the blurred background image to the one on home.py
 # Update login and register pages (some kind of ceiling on login label which binds it and the input fields from reaching a certain height)
 # Debug game logic and visuals (check resume_game, reset, and home.py)
 # Add api.py and integrate it
