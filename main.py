dc084ec3d214de265eafd1b1afa5a1a7d2185a86
AIM-Gaming <sonealpageot@gmail.com>
2026-09-12 19:30:40 -0400

    Removed bug note on main

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from assets import extract_video, extract_assets
 
 """
     Current bugs:
-        - Issue with linking bible references with their corresponding correct answers
         - The background music's MediaPlayer restarts when entering OptionsScreen because 
             on_enter() stops the current and triggers a new MediaPlayer instance even
             when the music currently playing is the same as the one selected in the music_selector
